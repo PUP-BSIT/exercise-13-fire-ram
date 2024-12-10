dfc8a582d0145da9b92dddf18c7cbf533917d9ee
barcelos-kevin <barceloskevinjoseph@gmail.com>
2024-12-11 07:44:26 +0800

    Add menu to main.py - Barcelos

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+os.system('cls')
+print("==============> Fire Ram <=================")
+print("(1) Kevin Joseph V. Barcelos")
+print("(2) Marc D. Veslino")
+print("(3) Kirby G. Consultado")
+print("(4) Gener A. Andaya")
+print("(5) Edriane O. Piadozo")
+user_choice = int(input("Enter your choice: "))
+
+match user_choice:
+    case 1:
+        pass
+    case 2:
+        pass 
+    case 3:
+        pass
+    case 4:
+        pass
+    case 5:
+        pass
