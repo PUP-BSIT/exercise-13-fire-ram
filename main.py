812a3dd40ac2e279e1d40264d90f48252d9603b8
barcelos-kevin <barceloskevinjoseph@gmail.com>
2024-12-11 08:38:12 +0800

    Add changes, loop the menu - Barcelos

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import os
 
-os.system('cls')
-print("==============> Fire Ram <=================")
-print("(1) Kevin Joseph V. Barcelos")
-print("(2) Marc D. Veslino")
-print("(3) Kirby G. Consultado")
-print("(4) Gener A. Andaya")
-print("(5) Edriane O. Piadozo")
-user_choice = int(input("Enter your choice: "))
+while True:
+    os.system('cls')
+    print("===============> Fire Ram <===============")
+    print("(1) Kevin Joseph V. Barcelos")
+    print("(2) Marc D. Veslino")
+    print("(3) Kirby G. Consultado")
+    print("(4) Gener A. Andaya")
+    print("(5) Edriane O. Piadozo")
+    user_choice = input("Enter your choice: ")
 
-match user_choice:
-    case 1:
-        pass
-    case 2:
-        pass 
-    case 3:
-        pass
-    case 4:
-        pass
-    case 5:
-        pass
+    if user_choice == '0':
+        break
+    
+    match user_choice:
+        case '1':
+            pass
+        case '2':
+            pass 
+        case '3':
+            pass
+        case '4':
+            pass
+        case '5':
+            pass
